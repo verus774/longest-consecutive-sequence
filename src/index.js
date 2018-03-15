ed10261328b070c85aa1ca1c20662c9babf79cec
Sergey Ladutko <verus774@mail.ru>
2018-03-15 22:50:36 +0300

    feature: implement longestConsecutiveLength function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+    let counter = 1;
+    let longestCounter = 1;
+
+    array.sort((a, b) => a - b);
+
+    if (array.length === 0) return 0;
+    if (array.length === 1) return 1;
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] === array[i - 1]) continue;
+        if (array[i] - array[i - 1] === 1) {
+            counter++;
+        } else {
+            counter = 1;
+        }
+        longestCounter = Math.max(counter, longestCounter);
+    }
+    return longestCounter;
+};
